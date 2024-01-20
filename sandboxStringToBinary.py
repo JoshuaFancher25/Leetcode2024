eb17a43decf12a502625c2289ad4d46b0be4f8a9
joshuafancher25 <bigdecimaljosh@gmail.com>
2024-01-20 13:32:56 -0800

    updates

---
+++ sandboxStringToBinary.py
@@ -0,0 +1,23 @@
+# string to binary
+x = "111"
+y = "1"
+
+a = int(x,2)
+b = int(y,2)
+print(a+b)
+
+# decimal is a base 10 counting system and the most popular counting system
+# for everyday commerce
+
+
+# we have the int function, takes two positional arguments - string, base
+
+print(int("FF",16))
+
+# bin function returns the binary for of an integer with the leading Ob
+# binary is a base 2 counting system
+print(bin(10))
+
+# hex - returns the hexadecimal value of an integer with the Ox prefix
+# hexadecimal is a base 16 counting system
+print(hex(16))
