219efbf5fc3344dfd2f6de6d792b72391b5b2efe
joshuafancher25 <bigdecimaljosh@gmail.com>
2024-01-27 19:14:39 -0800

    updates

---
+++ sandboxlower.py
@@ -0,0 +1,9 @@
+# using lower
+
+word = "AAVDadEAVEaAVEvdb"
+print(word.lower())
+for letter in word:
+    print(letter.lower())
+
+# can use the enhanced for loop to step through an iterable data structure
+
