8e3ff1cb49db84395946efbcf944e32c23f32747
JoshuaFancher25 <137542280+JoshuaFancher25@users.noreply.github.com>
2024-02-17 16:04:15 -0800

    updates

---
+++ sandboxANDOR.py
@@ -0,0 +1,6 @@
+#looking at the AND and OR operators in python
+print(False and False)
+
+print(False and True)
+
+print(True and True)
---
+++ sandboxJoin.py
@@ -0,0 +1,15 @@
+# learning how to use .join in python
+
+word1 = ["ab", "c", "defg"]
+print("hello".join(word1))
+
+# join takes and iterable such as a list and joins it together with separated by
+# the specified join character
+
+# really only works on str types
+
+theset = set(["a","d","f","z"])
+
+print(theset)
+print("--".join(theset))
+
