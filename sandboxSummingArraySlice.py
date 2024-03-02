1084366e10d00fa355bc6d8ca3f45179133be446
JoshuaFancher25 <137542280+JoshuaFancher25@users.noreply.github.com>
2024-03-01 17:16:43 -0800

    updates

---
+++ sandboxSummingArraySlice.py
@@ -0,0 +1,9 @@
+# learning how to sum an array slice
+arr = [1,2,3,4,5,6,7,8,9]
+print(sum(arr[:2]))
+print(sum(arr[6:]))
+print(sum(arr[1:2]))
+print(sum(arr[:0]))
+
+arr2 = [-1,-1,-1,-1,-1,1]
+print(sum(arr2[1:]))
