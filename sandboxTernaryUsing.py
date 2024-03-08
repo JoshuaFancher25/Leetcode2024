2f5b5568eee6273ddb3aff8d2c42a4907d14d580
JoshuaFancher25 <137542280+JoshuaFancher25@users.noreply.github.com>
2024-03-08 15:48:46 -0800

    updates

---
+++ sandboxTernaryUsing.py
@@ -0,0 +1,5 @@
+## using Ternary operators in python
+
+# ternary in python we use [return value IF condition Else return value]
+val = 101
+print("val is less than or equal to 100 " if val <= 100 else "val greater than 100")
