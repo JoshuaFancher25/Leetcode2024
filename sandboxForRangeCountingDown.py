8f876a9fd07e3428fd8d69405996022bd7ae43a9
JoshuaFancher25 <137542280+JoshuaFancher25@users.noreply.github.com>
2024-03-17 10:14:11 -0700

    updates

---
+++ sandboxForRangeCountingDown.py
@@ -0,0 +1,8 @@
+# counting down in a for looop
+# for loop by default loop upwards with a step of 1
+# must specify the negative step if needed
+for i in range(10, 0, -1):
+    print(i)
+
+# must remember the step value when using a
+
