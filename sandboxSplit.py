ad109972b9a17812022b933c112b18d57cd4cf37
JoshuaFancher25 <137542280+JoshuaFancher25@users.noreply.github.com>
2024-04-15 17:04:45 -0700

    updates

---
+++ sandboxSplit.py
@@ -0,0 +1,7 @@
+# learning the split method in python
+word = "abc:def:ghi:jkl:mno:pqr:stu:vwx:yz"
+arrword = word.split(":")
+print(word)
+print(arrword)
+for segment in arrword:
+    print(segment)
